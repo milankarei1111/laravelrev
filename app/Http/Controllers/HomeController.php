7aabdf66c110384a9bf1576aa83abb55ac2835c8
wanru_tsai <milankarei1111@gmail.com>
2019-12-03 16:21:26 +0800

    add 依登入帳號判斷角色的頁面

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,18 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Sentinel::findById(Auth::id());
+        if ($user) {
+            Sentinel::login($user);
+            if ($user == Sentinel::check()) {
+                if (Sentinel::inRole('Admin')) {
+                    return view('admin');
+                } else if (Sentinel::inRole('Boss')) {
+                    return view('boss');
+                } else {
+                    return view('user');
+                }
+            }
+        }
     }
 }
