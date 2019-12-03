c1e231fd31c8b53971666b083023370288ed117e
wanru_tsai <milankarei1111@gmail.com>
2019-12-03 16:09:22 +0800

    modify home view add admin view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,6 @@
                     @yield('admin.card')
                     @yield('boss.card')
                     @yield('user.card')
-                {{-- <div class="card-header">User Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    一般使用者登入頁面
-                </div> --}}
             </div>
         </div>
     </div>
