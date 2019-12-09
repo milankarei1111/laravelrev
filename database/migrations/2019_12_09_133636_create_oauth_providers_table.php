77563a30b0dbb548be1db6eee592620d61c85faf
wanru_tsai <milankarei1111@gmail.com>
2019-12-09 15:20:02 +0800

    更正nullable()寫錯，並執行 php artisan migrate

--- database/migrations/2019_12_09_133636_create_oauth_providers_table.php
+++ database/migrations/2019_12_09_133636_create_oauth_providers_table.php
@@ -18,8 +18,8 @@ class CreateOauthProvidersTable extends Migration
             $table->bigInteger('user_id');
             $table->string('provider');
             $table->string('provider_id')->index();
-            $table->string('token')->nullable()();
-            $table->string('refresh_token')->nullable()();
+            $table->string('token')->nullable();
+            $table->string('refresh_token')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')
