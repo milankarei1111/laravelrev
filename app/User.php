5086860fea7d062127bc88037177c59300ae1ebf
wanru_tsai <milankarei1111@gmail.com>
2019-12-09 13:44:21 +0800

    User 新增關聯oauth_providers表格

--- app/User.php
+++ app/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable implements JWTSubject
       return [];
     }
 
+    public function oauthProviders()
+    {
+        return $this->hasMany(User::class);
+    }
+
 }
