a3a611cb4748987cd9fef4ba19ec7d7ff63f5672
wanru_tsai <milankarei1111@gmail.com>
2019-12-10 15:42:30 +0800

    新增UserController控制API資料存取

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = QueryBuilder::for(User::class)
+            ->allowedFields('id')
+            ->allowedFilters(['id', 'name', 'email'])
+            ->allowedSorts(['id', 'email'])
+            ->allowedAppends(['fullname'])
+            ->allowedIncludes('oauthProviders')
+            ->get();
+        return $users;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $users = User::find($id);
+        return $users;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function sendResponse()
+    {
+        
+    }
+}
