8539e1a27b16f60473869c0f04a3fcd8936c6438
wanru_tsai <milankarei1111@gmail.com>
2019-12-10 09:31:11 +0800

    更正關聯模型

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\OAuthProvider;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -60,7 +61,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function oauthProviders()
     {
-        return $this->hasMany(User::class);
+       return $this->hasMany(OAuthProvider::class);
     }
 
 }
