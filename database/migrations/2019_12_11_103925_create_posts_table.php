754853edf9929aca187deecdc924dcc625d8012a
wanru_tsai <milankarei1111@gmail.com>
2019-12-11 10:48:38 +0800

    1.新增POST模型+遷移文件 指令: php artisan make:model Models/Post --migration
    2.配置遷移文件 2019_12_11_103925_create_posts_table
    
    3.執行遷移 php artisan migrate => 資料庫產生post表格

---
+++ database/migrations/2019_12_11_103925_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('post_id');
+            $table->text('title_target');
+            $table->string('title');
+            $table->text('content');
+            $table->bigInteger('category_id');
+            $table->string('image');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
